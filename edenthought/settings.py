5c3ae6c2c3fb834e61ce357c4745ed9daf0392a4
sten.r <46383486+stennn2001@users.noreply.github.com>
2024-02-12 12:20:59 +0200

    Update settings.py

--- edenthought/settings.py
+++ edenthought/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["django-app-thoughts.onrender.com","*"]
 
-#CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ["https://django-app-thoughts.onrender.com"]
 
 # Application definition
 
